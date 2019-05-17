b16557f97f74c43464be9ca45e245c6100a34068
Thierry Moudiki <thierry.moudiki@gmail.com>
2019-05-17 12:10:05 +0200

    commit before split

--- R/matern32.R
+++ R/matern32.R
@@ -64,6 +64,7 @@ fit_matern32 <- function(x, y, lambda = 10^seq(-5, 4, length.out = 100),
   n <- dim(x)[1]
   p <- dim(x)[2]
   stopifnot(n == length(y))
+  
   if (is.null(l))
     l <- sqrt(p)
   
@@ -123,45 +124,87 @@ fit_matern32 <- function(x, y, lambda = 10^seq(-5, 4, length.out = 100),
   
   if (method == "chol")
   {
-    stopifnot(length(lambda) <= 1)
-    K_plus <- K + lambda*diag(n)
-    invK <- chol2inv(chol(K_plus))
-    coef <- invK%*%centered_y
-    looe <- drop(coef/diag(invK))
+    if (length(lambda) <= 1)
+    {
+      
+      K_plus <- K + lambda*diag(n)
+      invK <- chol2inv(chol(K_plus))
+      coef <- invK%*%centered_y
+      loocv <- drop(coef/diag(invK))
+      
+    } else { # length(lambda) > 1
+      
+      get_loocv <- function(lambda)
+      {
+        K_plus <- K + lambda*diag(n)
+        invK <- chol2inv(chol(K_plus))
+        coef <- invK%*%centered_y
+        
+        return(drop(coef/diag(invK)))
+      }
+      
+    }
   }
   
   if (method == "eigen")
   {
-    stopifnot(length(lambda) <= 1)
-    eigenK <- base::eigen(K)
-    eigen_values <- eigenK$values
-    Q <- eigenK$vectors 
-    inv_eigen <- solve_eigen(Eigenvectors = Q,
-                Eigenvalues = eigen_values,
-                y = centered_y,
-                lambda = lambda)
-    
-    coef <- inv_eigen$coeffs
-    looe <- inv_eigen$loocv
+    if(length(lambda) <= 1)
+    {
+      
+      eigenK <- base::eigen(K)
+      eigen_values <- eigenK$values
+      Q <- eigenK$vectors 
+      inv_eigen <- solve_eigen(Eigenvectors = Q,
+                               Eigenvalues = eigen_values,
+                               y = centered_y,
+                               lambda = lambda)
+      
+      coef <- inv_eigen$coeffs
+      loocv <- inv_eigen$loocv 
+      
+    } else { # length(lambda) > 1
+      
+      get_loocv <- function(lambda)
+      {
+        eigenK <- base::eigen(K)
+        eigen_values <- eigenK$values
+        Q <- eigenK$vectors 
+        inv_eigen <- solve_eigen(Eigenvectors = Q,
+                                 Eigenvalues = eigen_values,
+                                 y = centered_y,
+                                 lambda = lambda)
+        
+        return(inv_eigen$loocv)
+      }
+      
+    }
   }
   
   if (method %in% c("chol", "svd"))
   {
-    centered_y_hat <- K %*% coef
-    fitted_values <- drop(ym +  centered_y_hat)
-    resid <- centered_y - centered_y_hat
-    
-    RSS <- sum((y - fitted_values)^2)
-    TSS <- sum((y - ym)^2)
-    R_Squared <- 1 - RSS/TSS
-    
-    return(list(K = K, l = l, 
-                coef = drop(coef), scales = x_scaled$xsd,
-                ym = ym, xm = x_scaled$xm,
-                fitted_values = fitted_values, resid = drop(resid),
-                looCV = looe, R_Squared = R_Squared, 
-                scaled_x = X, centered_y = centered_y)) 
+    if (length(lambda) == 1)
+    {
+      centered_y_hat <- K %*% coef
+      fitted_values <- drop(ym +  centered_y_hat)
+      resid <- centered_y - centered_y_hat
+      
+      RSS <- sum((y - fitted_values)^2)
+      TSS <- sum((y - ym)^2)
+      R_Squared <- 1 - RSS/TSS
+      
+      return(list(K = K, l = l, 
+                  coef = drop(coef), scales = x_scaled$xsd,
+                  ym = ym, xm = x_scaled$xm,
+                  fitted_values = fitted_values, resid = drop(resid),
+                  loocv = loocv, R_Squared = R_Squared, 
+                  scaled_x = X, centered_y = centered_y))  
+      
+    } else { # length(lambda) > 1
+      
+      
+    }
   }
+  
 }
 
 
